80e304fdf00461b0481ae89a78bddf490acb2b4d
userinterview <loizani@aol.com>
2021-06-18 15:45:01 +0200

    correction eslint espace autour de la virgule dans fichier source

--- LoizObsStudy.ts
+++ LoizObsStudy.ts
@@ -7,16 +7,16 @@ async function getPromiseFinalResult() : Promise<Response> {
 	//let promiseListeCVEs : Promise<Response> ;
 	//const promiseListeCVEs = fetch("https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex=1&resultsPerPage=2&cpeMatchString=cpe:2.3:*:microsoft" 
 	const promiseListeCVEs = fetch("https://jsonplaceholder.typicode.com/todos/1" 	
-	,{method: "GET"}
+	, {method: "GET"}
 	).then((res) => {   const resJson = res.json() ;
-						console.log('09/06/2021 - 17h08 : async function getPromiseFinalResult - dans le then : ', resJson ) ; 						
+						console.log('09/06/2021 - 17h08 : async function getPromiseFinalResult - dans le then : ' , resJson ) ; 						
 						return resJson ;} 
 		  )
-	 .catch(function(argError) { console.log("Erreur de recupération des produits pour le vendeur microsoft. L'erreur vaut : ", argError); }) ;
+	 .catch(function(argError) { console.log("Erreur de recupération des produits pour le vendeur microsoft. L'erreur vaut : " , argError); }) ;
 	
 	 
 	 const result = await promiseListeCVEs ;
-	 console.log('async function getPromiseFinalResult - resultat renvoyé : ', result ) ; 
+	 console.log('async function getPromiseFinalResult - resultat renvoyé : ' , result ) ; 
 	 return result ;	  
 }
 
@@ -30,7 +30,7 @@ const objObserv :  Observable<Promise<Response>> =  new Observable(function(argo
 
 getTypeScriptObservable().subscribe(
     function(resultObservable) {
-    console.log("getTypeScriptObservable - resultat : ", resultObservable) ;
+    console.log("getTypeScriptObservable - resultat : " , resultObservable) ;
     }
 );
 
